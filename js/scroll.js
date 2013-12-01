d2bd69d8afaad49fc59777221ad42ba8a56512fc
laurenbenichou <laurenbenichou@gmail.com>
2013-11-30 18:26:32 -0800

    Adding pages

---
+++ js/scroll.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+  function scrollToAnchor(anchor){
+    var aTag = $("a[name='"+ anchor +"']");
+    $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+  }
+
+  $("a[href='#webdev']").click(function() {
+    scrollToAnchor('webdev');
+  });
+
+  $("a[href='#journo']").click(function() {
+    scrollToAnchor('journo');
+  });
+
+  $("a[href='#about']").click(function() {
+    scrollToAnchor('about');
+  });
+
+});
